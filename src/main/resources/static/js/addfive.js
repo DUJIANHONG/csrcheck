c5d1ed3d970e9934820e20cd65393a4add20c706
DUJIANHONG <2387587314@qq.com>
2019-11-23 10:44:57 +0800

    Five_Supplier中announcement_date改为string

--- src/main/resources/static/js/addfive.js
+++ src/main/resources/static/js/addfive.js
@@ -3,10 +3,7 @@ $().ready(function() {
     loadType();
     validateRule();
 });
-jQuery.validator.addMethod("announcement_dates",function(value,element){
-    var announcement=/^[0-9]*$/;
-    return this.optional(element) || (announcement).test(value);
-},'请输入数字');
+
 //添加
 $("#save").click(function () {
     if($("#company_id").val()==null||$("#company_id").val()=='') {
@@ -38,17 +35,11 @@ function validateRule() {
         rules : {
             name : {
                 required : true
-            },
-            announcement_date:{
-                announcement_dates:true
             }
         },
         messages : {
             name : {
                 required : icon + ""
-            },
-            announcement_date:{
-                announcement_dates: "请输入数字"
             }
         },
         errorPlacement:function(error,element){
