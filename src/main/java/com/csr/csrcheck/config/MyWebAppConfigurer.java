c287af066d959ac1fa3c1f6982e92231f1046b8f
DUJIANHONG <2387587314@qq.com>
2019-12-05 10:03:23 +0800

    sadasd

--- src/main/java/com/csr/csrcheck/config/MyWebAppConfigurer.java
+++ src/main/java/com/csr/csrcheck/config/MyWebAppConfigurer.java
@@ -4,15 +4,6 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
-/**
- * @description:
- * @author: Administrator
- * @date: Created in 2019/12/5 9:58
- * @version:
- * @modified By:
- */
-public class MyWebMvcConfigurer {
-
     /**
      * 资源映射路径
      */
@@ -23,4 +14,3 @@ public class MyWebMvcConfigurer {
             registry.addResourceHandler("/temp-rainy/**").addResourceLocations("file:D:/temp-rainy/");
         }
     }
-}
